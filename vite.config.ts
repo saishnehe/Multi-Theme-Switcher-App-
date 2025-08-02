edb5ea4eb8130dec83845f42d86af3af3aa11618
Saish Nehe <saishnehe48@gmail.com>
2025-08-02 11:07:03 +0530

    Removed Tailwind CSS from project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace "your-username" and "repo-name" correctly
 export default defineConfig({
-  base: '/multi-theme-switcher-app/',
+    base: '/multi-theme-switcher-app/',
+
   plugins: [react()],
 });
